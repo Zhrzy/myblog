181bf781bc0480004ddfa95619517bd25536f163
Zhrzy <zhanghaorenzhr@163.com>
2020-03-15 14:17:23 +0800

    功能完善

---
+++ blog_admin/src/main/java/com/zy/myblog/admin/constant/SysConstant.java
@@ -0,0 +1,19 @@
+package com.zy.myblog.admin.constant;
+
+/**
+ * 系统常量
+ * @author zy 1716457206@qq.com
+ */
+public final class SysConstant {
+    /*1.返回值常量*/
+    public final static String SUCCESS = "success";
+    public final static String ERROR = "error";
+
+
+    /*2.SQL字段常量*/
+    public final static String UID = "uid";
+    public final static String USERNAME = "user_name";
+    public final static String EMAIL = "email";
+    public final static String MOBILE = "mobile";
+
+}
