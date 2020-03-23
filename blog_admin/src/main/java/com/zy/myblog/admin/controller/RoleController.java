1f3a495e432f0659f07c12cb4aec40010fd99b96
Zhrzy <zhanghaorenzhr@163.com>
2020-03-24 00:18:55 +0800

    动态部署：增加blog_admin 的dockerfile 修改配置文件

--- blog_admin/src/main/java/com/zy/myblog/admin/controller/RoleController.java
+++ blog_admin/src/main/java/com/zy/myblog/admin/controller/RoleController.java
@@ -3,8 +3,10 @@ package com.zy.myblog.admin.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.zy.myblog.admin.service.CategoryMenuService;
 import com.zy.myblog.admin.service.RoleService;
 import com.zy.myblog.xx.entity.Admin;
+import com.zy.myblog.xx.entity.CategoryMenu;
 import com.zy.myblog.xx.entity.Role;
 import com.zy.myblog.xx.utils.ResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author zy 1716457206@qq.com
@@ -22,6 +27,7 @@ import java.util.List;
 public class RoleController {
     @Autowired
     private RoleService roleService;
+    private CategoryMenuService menuService;
 
 
     @RequestMapping("/getRoleList")
@@ -36,6 +42,13 @@ public class RoleController {
         page.setCurrent(currentPage);//页数
         page.setSize(pageSize);//每页条数
         IPage<Role> pageList = roleService.page(page, queryWrapper);//根据分页参数和条件构造器查询
+        List<Role> list  = page.getRecords();
+        list.forEach(role -> {
+
+
+        });
+        pageList.setRecords(list);
+
         return ResultUtil.result("success", "分页查询成功", pageList);
     }
 }
